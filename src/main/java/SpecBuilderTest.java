68f414e5caad7d5fe141f393941bbd0f55d9de0f
Sneha Natekar <snehanatekar@Snehas-MacBook-Pro.local>
2026-01-18 12:28:33 +0530

    specbuilder

---
+++ src/main/java/SpecBuilderTest.java
@@ -0,0 +1,58 @@
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.List;
+import java.util.*;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import pojo.AddPlace;
+import pojo.Location;
+
+public class SpecBuilderTest {
+
+	@Test
+
+	public void serialization() {
+
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
+
+		AddPlace p = new AddPlace();
+		p.setAccuracy(50);
+		p.setName("Frontline house");
+		p.setPhone_number("(+91) 983 893 3937");
+		p.setAddress("29, side layout, cohen 09");
+		p.setLanguage("French-IN");
+		p.setWebsite("http://google.com");
+
+		List<String> myList = new ArrayList<String>();
+		myList.add("abc");
+		myList.add("pqr");
+		p.setTypes(myList);
+
+		Location l = new Location();
+		l.setLat(-38.383494);
+		l.setLng(33.427362);
+		p.setLocation(l);
+
+		RequestSpecification req = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+				.addQueryParam("key", "qaclick123").setContentType(ContentType.JSON).build();
+
+		ResponseSpecification resspec = new ResponseSpecBuilder().expectStatusCode(200)
+				.expectContentType(ContentType.JSON).build();
+		
+		RequestSpecification res = given().spec(req).body(p);
+
+		Response response = res.when().post("/maps/api/place/add/json").then().spec(resspec)
+				.extract().response();
+
+		String responseString = response.asString();
+		System.out.println(responseString);
+	}
+}
